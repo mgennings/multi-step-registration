4610435e6bd8eb5e7c6062e2ca00ec5b1ff069b0
Matthew Gennings <matthewgennings@gmail.com>
2022-06-16 12:20:36 -0500

    feat: added child routes for each step

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterRoutingModule } from './pages/register/register-routing.module';
+import { ConfirmComponent } from './pages/confirm/confirm.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: 'register', loadChildren: () => RegisterRoutingModule },
+  { path: 'confirm', component: ConfirmComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/register/register-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register.component';
+import { StepOneComponent } from '../../components/step-one/step-one.component';
+import { StepTwoComponent } from '../../components/step-two/step-two.component';
+import { StepThreeComponent } from '../../components/step-three/step-three.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '',
+        component: RegisterComponent,
+        children: [
+          { path: 'step-one', component: StepOneComponent },
+          { path: 'step-two', component: StepTwoComponent },
+          { path: 'step-three', component: StepThreeComponent },
+        ],
+      },
+    ]),
+  ],
+  exports: [RouterModule],
+})
+export class RegisterRoutingModule {}
